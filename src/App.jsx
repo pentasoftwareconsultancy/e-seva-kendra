2450c15cd7706f4913d4233efd2c389efb957106
Nitin204 <nitinkatore686@gmail.com>
2026-02-09 10:31:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import React from 'react'
+import Home from './pages/home.jsx'
+function App() {
+  
+
+  return (
+    <>
+     <Home />
+    </>
+  )
+}
+
+export default App
