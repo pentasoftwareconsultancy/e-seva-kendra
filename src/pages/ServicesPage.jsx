0574268ee84546124422938cdacd977effe06540
JainSrushti <srushpjain2811@gmail.com>
2026-02-17 11:34:08 +0530

    services complete

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Pan from '../components/services/Pan'
+// import React from 'react'
+// import PANCardServices from '../components/services/PANCardServices'
+// import AadhaarForm from '../components/Services/AadhaarForm';
 
-function ServicesPage() {
-  return (
-    <div>
-      <Pan />
-    </div>
-  )
-}
+// function ServicesPage() {
+//   return (
+//     <div>
+//       <PANCardServices />
+//       <AadhaarForm/>
+//     </div>
+//   )
+// }
 
-export default ServicesPage;
+// export default ServicesPage;
